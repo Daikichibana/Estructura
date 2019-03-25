25865fc994f1c541ebd77239c339d14433eb53e1
Daiki <42224652+Daikichibana@users.noreply.github.com>
2019-03-25 18:56:09 -0400

    Create vectores.cpp

---
+++ practico 3/vectores.cpp
@@ -0,0 +1,55 @@
+#include "stdafx.h"
+#include <iostream>
+#include "Vectores.h"
+using namespace std;
+
+void main(){
+Vector a,b,c;
+int num, resp, pos;
+/*
+//Ejercicio 24
+a.leer("A");
+cout<<"Ingrese el numero";
+cin>>num;
+resp = a.frecuencia(num);
+cout<<"El resultado es: "<<resp<<endl;
+
+//Ejercicio 25
+a.leer("A");
+cout<<"de el valor: ";
+cin>>num;
+if(a.insertar(num) != true){cout<<" vector lleno o posicion mala"<<endl;}
+a.mostrar("A");
+
+//Ejercicio 26
+a.leer("A");
+cout<<"Que posicion quiere eliminar? ";
+cin>>num;
+a.eliminar(num);
+a.mostrar("A");
+
+//Ejercicio 27
+a.leer("A");
+cout<<"Digito: ";
+cin>>num;
+
+resp= a.busqueda(num);
+cout<<"El resultado es: "<<resp<<endl;
+
+//Ejercicio 28
+
+//Ejercicio 29
+a.leer("A");
+a.recorrer();
+a.mostrar("C");
+
+//Ejercicio 30
+a.leer("A");
+a.recorrer2();
+a.mostrar("C");
+*/
+
+
+
+system("pause");
+}
